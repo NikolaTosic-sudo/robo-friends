59023d7cad480feaeceff31c51db844c33591152
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-08-03 15:38:05 +0200

    Adding State
    Adding State to this simple app to filter robots aka Search them

--- src/App.js
+++ src/App.js
@@ -3,17 +3,36 @@ import './App.css';
 import CardList from "./CardList";
 import SearchBox from './SearchBox';
 import 'tachyons';
+import {robots} from "./robots";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchField: ''
+    }
+  }
 
+  onSearchChange = (event) => {
+
+    this.setState({
+      searchField: event.target.value
+    })
+
+  }
 
   render() {
+
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    })
 
     return (
       <div className="App tc">
         <h1 className="tc tl">Robo Friends</h1>
-        <SearchBox />
-        <CardList/>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots}/>
       </div>
     );
 }}
