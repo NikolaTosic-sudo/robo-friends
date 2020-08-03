678c19cc9696a1c191cf6565d2198847ca8385a2
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-08-03 14:32:39 +0200

    Creating Cards
    Creating cards with some 'dynamic' data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
+import Card from "./Card";
+import 'tachyons';
+import {robots} from './robots';
 
 class App extends Component {
 
@@ -8,7 +11,12 @@ class App extends Component {
 
     return (
       <div className="App">
-
+        <h1 className="tc tl">Robo Friends</h1>
+        <div>
+          <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+          <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+          <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+        </div>
       </div>
     );
 }}
