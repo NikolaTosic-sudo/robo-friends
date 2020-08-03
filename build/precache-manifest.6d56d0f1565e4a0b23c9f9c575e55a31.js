b0d455ad8d84032036ea118970a3b69ac007cda5
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-08-03 17:48:13 +0200

    Build Production

---
+++ build/precache-manifest.6d56d0f1565e4a0b23c9f9c575e55a31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307a73723d121f7a828c3cf625089db1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53d6cc2b77e303cc88e6",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4070d0ed80fb7f7fc411",
+    "url": "/static/css/main.0d7f0405.chunk.css"
+  },
+  {
+    "revision": "53d6cc2b77e303cc88e6",
+    "url": "/static/js/2.2cdae6f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2cdae6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4070d0ed80fb7f7fc411",
+    "url": "/static/js/main.f24057f8.chunk.js"
+  },
+  {
+    "revision": "0e95140b7bed8b651455",
+    "url": "/static/js/runtime-main.318786ac.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  }
+]);
