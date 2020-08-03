4fd2137341a6c8a3fe3c35ad5972675d985daa37
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-08-03 16:28:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,15 +40,21 @@ class App extends Component {
       return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
     })
 
-    return (
-      <div className="tc">
-        <h1 className="f1">Robo Friends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <Scroll>
-          <CardList robots={filteredRobots}/>
-        </Scroll>
-      </div>
-    );
-}}
+    if(this.state.robots.length === 0) {
+
+      return <h1>Loading...</h1>
+
+    } else {
+
+        return (
+          <div className="tc">
+            <h1 className="f1">Robo Friends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <Scroll>
+              <CardList robots={filteredRobots}/>
+            </Scroll>
+          </div>
+        );
+}}}
 
 export default App;
