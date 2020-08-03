f81d3fcf083f3ba192e83a3eb06978d2064445e6
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-08-03 15:10:16 +0200

    Adding a Search Box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import CardList from "./CardList";
+import SearchBox from './SearchBox';
 import 'tachyons';
 
 class App extends Component {
@@ -9,8 +10,9 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App">
+      <div className="App tc">
         <h1 className="tc tl">Robo Friends</h1>
+        <SearchBox />
         <CardList/>
       </div>
     );
