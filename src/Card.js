e0ae0fbb455f88a92dc0bf57dfdf654b1bf1e075
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-08-21 11:55:47 +0200

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Chuck from './Chuck';
+import "./Card.css"
 
 const Card = ({ name, email, id }) => {
 
     return (
 
-        <div className='tc bg-light-green dib br3 pa3 grow shadow-5 fl ma3'>
+        <div className='tc bg-light-green dib br3 pa3 grow shadow-5 fl ma3 yes'>
             <img src={`https://robohash.org/${id}?180x180`} alt="img"/>
             <div>
                 <h2>{name}</h2>
