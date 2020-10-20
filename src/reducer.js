8169a0bd1337beae03a822a7b14b86f8affe948e
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-10-20 13:48:23 +0200

    Update reducer.js
    Reducer for fetching robots and new initialState for that reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,40 @@
-import {CHANGE_SEARCH_FIELD} from "./constants";
+import {
+    CHANGE_SEARCH_FIELD,
+    FETCH_ROBOTS_PENDING,
+    FETCH_ROBOTS_SUCCESS,
+    FETCH_ROBOTS_FAIL
+} from "./constants";
 
-const initialState = {
+
+const initialStateSearch = {
     searchField: ''
-}
+};
 
-export const searchRobots = (state = initialState, action = {}) => {
+export const searchRobots = (state = initialStateSearch, action = {}) => {
     switch (action.type) {
         case CHANGE_SEARCH_FIELD:
             return { ...state, searchField: action.payload};
         default:
             return state
+    }
+};
+
+const initialStateRobots = {
+    robots: [],
+    isPending: false,
+    error: ''
+};
+
+
+export const fetchRobots = (state = initialStateRobots, action) => {
+    switch (action.type) {
+        case FETCH_ROBOTS_PENDING:
+            return { ...state, isPending: true };
+        case FETCH_ROBOTS_SUCCESS:
+            return { ...state, robots: action.payload, isPending: false };
+        case FETCH_ROBOTS_FAIL:
+            return { ...state, error: action.payload, isPending: false };
+        default:
+            return state;
     }
 };
