c587983ef15204f3b3445c9fa07a1296e512362e
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-10-20 13:47:55 +0200

    Update index.js
    Creating rootReducer by combining reducers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
-import { searchRobots } from "./reducer";
+import { searchRobots, fetchRobots } from "./reducer";
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
+const rootReducer = combineReducers({searchRobots, fetchRobots});
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <React.StrictMode>
