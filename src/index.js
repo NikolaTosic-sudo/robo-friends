368f69b727517a03047669ecee5d350c05501e97
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-10-20 13:40:13 +0200

    Update index.js
    New middleware, thunkMiddleware from redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from "redux";
 import { createLogger } from 'redux-logger'
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
 import { searchRobots } from "./reducer";
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <React.StrictMode>
